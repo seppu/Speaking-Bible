0025da1f7f0492b9ededd3fbea227fcbcc0f2be4
Joseph Abraham <seppu6776@gmail.com>
2014-11-02 08:01:41 +0000

    Changes made by Joseph Abraham (seppu6776@gmail.com).

--- Speaking Bible/js/db_functions.js
+++ Speaking Bible/js/db_functions.js
@@ -7,8 +7,10 @@ function populateDB(tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS MEMVERSES (book_no, chapter_no, verse_no)');
     tx.executeSql('CREATE TABLE IF NOT EXISTS RECORDINGS (book_no,chapter_no,verse_no, path)');
 }
+function createBooks(tx) {
+    tx.executeSql('SELECT * FROM BOOKS', [], querySuccessBooks, errorCB);
+}
 
------
 function createChapters(tx) {
     
     tx.executeSql('SELECT * FROM CHAPTERS', [], querySuccessChapters, errorCB);
